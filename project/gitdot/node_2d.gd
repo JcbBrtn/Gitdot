cff720b8f4710c4677aaf57d339014bdef24f1e4
JcbBrtn <42615937+JcbBrtn@users.noreply.github.com>
2025-04-23 20:48:06 -0700

    Add quotes around export path

--- project/gitdot/node_2d.gd
+++ project/gitdot/node_2d.gd
@@ -3,11 +3,11 @@ extends Node2D
 var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
-	$RigidBody2D.apply_force(Vector2(rng.randf_range(-100, 100), rng.randf_range(-100, -10)))
+	$RigidBody2D.apply_force(Vector2(0, rng.randf_range(-10000, -1000)))
 	
 func _process(delta: float) -> void:
-	if abs($RigidBody2D.position.x) > get_viewport_rect().size.x:
-		$RigidBody2D.position.x *= -1
+	if abs($RigidBody2D.global_position.x) > get_viewport_rect().size.x / 2:
+		$RigidBody2D.global_position.x *= -1
 	
-	if $RigidBody2D.position.y > get_viewport_rect().end.y:
+	if $RigidBody2D.global_position.y > get_viewport_rect().end.y:
 		$RigidBody2D.global_position.y = 0 - get_viewport_rect().size.y / 2
